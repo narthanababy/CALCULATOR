b8e8cb43e9772c227b0e385177a6a8e33329fc25
NARTHANA BABY B S <119154115+narthanababy@users.noreply.github.com>
2025-09-29 17:20:58 +0530

    Caluculating_rent.py

--- Caluculating_rent.py
+++ Caluculating_rent.py
@@ -1,1 +1,13 @@
 
+
+rent =int(input("Enter your hostel/flat rent="))
+food =int(input("Enter the amount of food ordered is="))
+electricity = int(input("enter amount spend for electricity="))
+charge_per_unit =int(input("enter the charge per unit="))
+persons =int(input("enter the no: persons="))
+
+
+total_bill=electricity*charge_per_unit
+output=(rent+food+total_bill)//persons
+
+print("Each person will pay = "+str(output))
